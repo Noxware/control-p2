3e38b117c1000c2b2f556ad424eb111db17d9bae
Noxware <fprofeti98@gmail.com>
2021-04-29 21:53:50 -0300

    Random helper

---
+++ lib/util/extensions/file.dart
@@ -0,0 +1,3 @@
+import 'dart:io';
+
+extension NxFileExtension on File {}
---
+++ lib/util/helpers/random.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+Random _rnd = Random();
+
+/// Helper to work with random things
+class RandomHelper {
+  /// Get a random String of a specific length.
+  ///
+  /// If [chars] is not provided, this will use only safe chars from [_chars].
+  ///
+  /// Based on: https://stackoverflow.com/questions/61919395/how-to-generate-random-string-in-dart
+  static String getRandomString(int length, {String? chars = _chars}) {
+    return String.fromCharCodes(Iterable.generate(length, (_) {
+      return _chars.codeUnitAt(_rnd.nextInt(_chars.length));
+    }));
+  }
+}

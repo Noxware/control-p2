96f6e0ac74103bbc53a71f6e76b48f8205e390d7
Noxware <fprofeti98@gmail.com>
2021-06-13 21:06:42 -0300

    bug fix

--- lib/util/extensions/string.dart
+++ lib/util/extensions/string.dart
@@ -1,6 +1,10 @@
 extension NxStringExtension on String {
   String withVars(Map vars) {
-    return vars.entries.fold<String>(
+    // Sort like [b, aa, a], so replacing 'a' will not affect 'aa'.
+    final entries = vars.entries.toList();
+    entries.sort((e1, e2) => e2.key.toString().compareTo(e1.key.toString()));
+
+    return entries.fold<String>(
         this,
         (res, entry) => res.replaceAll(
             r'$' + entry.key.toString(), entry.value.toString()));
